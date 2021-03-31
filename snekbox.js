c250d8a1ad4593050002a963693b502635288ce1
OllieOllieAusum <ollieollieausum@gmail.com>
2021-03-31 11:44:22 -0500

    Something

--- snekbox.js
+++ snekbox.js
@@ -1,4 +1,5 @@
-var canvas = document.getElementById("snekbox");
+var canvas = document.getElementById("sneekbox");
+console.log(canvas)
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150, 75);
