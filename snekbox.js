30967c63f9cdf83d352473aa11f47a97963a7fd1
OllieOllieAusum <ollieollieausum@gmail.com>
2021-04-01 08:14:51 -0500

    Change website title, optimise grid draw loop

--- snekbox.js
+++ snekbox.js
@@ -7,14 +7,10 @@ function activecanvas(canvasid) {
 function drawgrid() {
   var x;
   var y;
+	ctx.fillStyle = "#474747";
   for (x = 3; x < canvasDOM.width; x = x + canvasDOM.width / 20) {
-    ctx.fillRect(x, 3, canvasDOM.width / 25, canvasDOM.height / 25);
     for (y = 3; y < canvasDOM.height; y = y + canvasDOM.height / 20) {
-
-      ctx.fillRect(x, y, canvasDOM.width / 25, canvasDOM.height / 25);
-      ctx.fillStyle = "#474747";
       ctx.fillRect(x + 4, y + 4, canvasDOM.width / 40, canvasDOM.height / 40);
-      ctx.fillStyle = "#000000";
     }
   }
 }
